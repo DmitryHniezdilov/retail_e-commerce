c23192f63cd9d5ceebf777409841a024e36480f2
DmitryHniezdilov <hniezdilovd@gmail.com>
2022-06-28 21:25:45 +0300

    add slider-products

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -70,4 +70,37 @@ $(document).ready(function () {
             }
         ]
     })
+
+    $(".js-slider-products").slick({
+        infinite: true,
+        arrows: true,
+        dots: false,
+        focusOnSelect: true,
+        autoplay: false,
+        autoplaySpeed: 5000,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1366,
+                settings: {
+                    slidesToShow: 3,
+                    dots: true,
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    })
 })
